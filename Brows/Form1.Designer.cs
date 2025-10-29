431c75c38c9964615a88a25a85243d0f156137b1
jules vannuijs <jules.vannuijs@bee-organisation.com>
2025-10-29 18:39:25 +0100

    Add project files.

---
+++ Brows/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Brows
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            SuspendLayout();
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Location = new Point(1, 47);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(946, 591);
+            webView21.Source = new Uri("https://www.buienradar.nl/nederland/neerslag/buienradar", UriKind.Absolute);
+            webView21.TabIndex = 0;
+            webView21.ZoomFactor = 1D;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(315, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(333, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(42, 23);
+            button1.TabIndex = 2;
+            button1.Text = "Go";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "www.classicalwcrb.org/", "www.npoklassiek.nl/online-radio-luisteren/gedraaid", "www.vrt.be/vrtmax/livestream/audio/klaracontinuo/", "www.bbc.co.uk/sounds/play/live/bbc_radio_three", "google.com" });
+            comboBox1.Location = new Point(432, 11);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(268, 23);
+            comboBox1.TabIndex = 3;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(860, 9);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 25);
+            button2.TabIndex = 4;
+            button2.Text = "Suggesties";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(815, 9);
+            button3.Name = "button3";
+            button3.Size = new Size(39, 25);
+            button3.TabIndex = 5;
+            button3.Text = ". . .";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(381, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(42, 23);
+            button4.TabIndex = 6;
+            button4.Text = "Add";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(947, 638);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(webView21);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Jules on Edge";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private TextBox textBox1;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ Brows/Form1.cs
@@ -0,0 +1,57 @@
+namespace Brows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Uri url = new Uri(textBox1.Text);
+            webView21.Source = url;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Kijk zo doe je dat als je je eigen content will weergeven in de browser.
+            // Je gebruikt dan de webView21.NavigateToString() property van de webview. De string tussen de () wordt dan in de browser
+            // weergegeven. In die string kun je ook html elementen zoals <br> schrijven. <br> is het html element om een break te maken zodat
+            // de rest van de string op een nieuwe regel wordt weergegeven. 
+            string suggesties = @"
+                Suggesties voor een portfolio desktop app die de browser control webview2 gebruikt om:<br>
+                muziek of podcasts af te spelen (radio app)<br>
+                een browser te maken met de styling van je fav voetbalclub.<br>
+                een browser te maken die je leeromgeving is met links naar tutorials, repo's vscode online etc.<br>
+                een browser te maken gericht op je hobby 
+                ";
+            webView21.NavigateToString(suggesties);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb1 = sender as ComboBox;
+            string url = "https://" + cb1.SelectedItem.ToString();
+            System.Uri uri = new Uri(url);
+            webView21.Source = uri;
+            textBox1.Text = url;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string strippedUrl = textBox1.Text.Substring(8);
+            comboBox1.Items.Add(strippedUrl);
+        }
+    }
+
+    public class Form2 : Form1
+    {
+        public Form2()
+        {
+            //Form2 form2 = new Form2();
+            //form2.Show();
+            this.Size = new System.Drawing.Size(555, 555);
+        }
+    }
+}
